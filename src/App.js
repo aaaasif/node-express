157ed24e7d0b0987a8e599414176b749bdef8676
aaaasif <alexasif4@gmail.com>
2021-10-24 20:58:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[users, setUsers] = useState([]);
+
+  useEffect( () =>{
+    fetch('http://localhost:5000/users')
+    .then(res =>res.json())
+    .then(data =>setUsers(data));
+
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Found users: {users.length}</h2>
     </div>
   );
 }
